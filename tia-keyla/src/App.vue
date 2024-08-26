a146114abf1e4174fc494fb0888d2f92f41e7e3a
Luiz Almeida <92050300+LuizAlmeida20@users.noreply.github.com>
2024-08-26 08:36:29 -0300

    Initial commit

---
+++ tia-keyla/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <web-site />
+  </v-app>
+</template>
+
+<script lang="ts">
+  import WebSite from "@/components/WebSite.vue";
+
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    components: {
+      WebSite
+    }
+  }
+</script>
+
+<style>
+  * {
+    background-color: white;
+  }
+</style>
---
+++ tia-keyla/src/components/WebSite.vue
@@ -0,0 +1,420 @@
+<template>
+  <nav
+    style="
+      display: flex;
+      align-items: center;
+      gap: 5%;
+      margin-left: 5%;
+      margin-top: 2%;
+    "
+  >
+    <button><img src="/src/assets/default_icon.png" alt="Logo da Tia Keyla" style="width: 80%; height: 50%;"></button>
+    <a href="#sobre_btn1"><button class="navbar_btn">Sobre</button></a>
+    <a href="#propostas"><button class="navbar_btn">Propostas</button></a>
+    <a href="#contatos"><button class="navbar_btn">Contato</button></a>
+  </nav>
+  <section
+    style="display: flex; align-items: center; padding-left: 8%; padding-right: 2%;"
+  >
+    <div>
+      <h1
+        style="
+          margin-bottom: 1.5%;
+          font-weight: bold;
+          line-height: 1;
+        "
+      >Pela Proteção das Crianças e Adolescentes</h1>
+      <a href="https://chat.whatsapp.com/Id7JGzhEKnGA6pbpbSqmu3" target="_blank" style="text-decoration: none">
+        <v-card
+          color="#f97a08"
+          rounded="lg"
+          height="7vh"
+          style="text-align: center; align-content: center"
+          width="50%"
+        >
+          <h3
+            style="background-color: #f97a08; font-weight: bold"
+          >
+            FAÇA PARTE DO NOSSO EXÉRCITO
+          </h3>
+        </v-card>
+      </a>
+    </div>
+    <img src="/src/assets/TiaKeyla.png" alt="Imagem de Tia Keyla - Número 22.123" style="height: 100%; width: 60%">
+  </section>
+  <v-card
+    color="#f97a08"
+    width="100%"
+    height="12vh"
+    style="
+      padding-right: 30%;
+      padding-left: 30%;
+      text-align: center;
+      align-content: center;
+      margin-bottom: 1%;
+    "
+  >
+    <h3 style="background-color: #f97a08; color: white; font-weight: bold;">
+      Para que todos vejam, e saibam, e considerem, e juntamente entendam que a mão do Senhor fez isto (Isaías 41:20)
+    </h3>
+  </v-card>
+  <div style="text-align: center; align-content: center; padding-right: 32%; padding-left: 32%; margin-top: 5%" id="sobre_btn1">
+    <h1
+      style="
+          margin-bottom: 5%;
+          font-weight: bold;
+          line-height: 1;
+        "
+    >Uma História de Superação e Dedicação</h1>
+    <p style="text-align: justify">
+      Sou Keyla Cristina,  esposa do Daniel e mãe do David e do Danilo, mas todos me conhecem como tia Keyla.
+      Sou pedagoga, especialista em educação emocional e sexual. Durante a minha infância, vivi um episódio de abuso que
+      marcou profundamente a minha vida. Esse trauma me inspirou a dedicar minha trajetória à prevenção de abusos e à
+      proteção de crianças e adolescentes. Foi assim que nasceu o projeto #PROTEGIDOS, que já impactou mais de 6 mil
+      pessoas em Contagem -MG. Nosso trabalho se concentra em capacitar profissionais e famílias na criação de espaços
+      seguros e amorosos para todas as crianças e a aplicação de oficinas lúdicas para que as crianças e adolescentes
+      saibam se proteger do abuso sexual ou qualquer outro tipo de  violência.
+    </p>
+    <div style="margin-top: 5%; margin-bottom: 10%" class="d-flex flex-column justify-content-between">
+      <a href="https://youtu.be/30jKGtOvJBM">Vídeo de depoimento 1</a>
+      <a href="https://youtu.be/nyAbqnLWEYU">Vídeo de depoimento 2</a>
+      <a href="https://youtu.be/M76HV_KIJMw">Vídeo de depoimento 3</a>
+    </div>
+  </div>
+  <div
+    style="padding-left: 15%; padding-right: 15%;"
+  >
+    <p
+      style="
+        font-size: 150%;
+        padding: 2% 12%;
+        border-top: #f97a08 solid 0.7rem;
+        border-bottom: #f97a08 solid 0.7rem;
+        border-left: #fc9028 solid 0.7rem;
+        border-right: #fc9028 solid 0.7rem
+        "
+    >
+      “Eleger-me é escolher uma vereadora comprometida com a proteção das crianças e adolescentes de Contagem.
+      Minha experiência, paixão, e dedicação à causa são a garantia de um futuro mais seguro e promissor para todos.
+      Juntos, podemos construir uma cidade onde as crianças sejam verdadeiramente valorizadas e protegidas.”
+    </p>
+  </div>
+  <div style="margin-top: 5%; margin-left: 5%;">
+    <h2
+      style="font-size: 300%; font-weight: bold"
+    >
+      Você sabia?
+    </h2>
+    <div style="display: flex; align-items: center; gap: 1%; margin-top: 2%; padding-right: 10%;">
+      <img src="/src/assets/62porcent.png" alt="Círculo 62% completo" style="width: 10%; height: 40%"/>
+      <p>
+        No Brasil, uma pessoa é <span class="span_strong">vítima</span> de abuso sexual
+        <span class="span_strong">a cada 6 minutos</span>, refletindo uma realidade alarmante.
+        Dentre essas vítimas, <span class="span_strong">62% têm até 13 anos</span>,
+        evidenciando a gravidade da situação entre as crianças e adolescentes.
+      </p>
+    </div>
+    <div style="display: flex; align-items: center; gap: 1%; margin-top: 2%; padding-right: 10%;">
+      <img src="/src/assets/6.5percent.png" alt="Círculo 62% completo" style="width: 13%; height: 60%"/>
+      <p>
+        Em 2023, foram registradas aproximadamente
+        <span class="span_strong">84 mil ocorrências de abuso sexual infantil</span>, o que representa um
+        <span class="span_strong">aumento de 6,5%</span> em comparação com o ano anterior, 2022.
+        Esse crescimento destaca a necessidade urgente de
+        intensificar as ações de prevenção e suporte para enfrentar essa grave questão.
+      </p>
+    </div>
+  </div>
+  <div class="d-flex align-center justify-center flex flex-column" style="margin-top: 2%">
+    <v-card
+      color="#f97a08"
+      rounded="lg"
+      height="5vh"
+      style="text-align: center; align-content: center; margin-bottom: 5%"
+      width="38%"
+    >
+      <h3
+        style="background-color: #f97a08; color: #ffffff; font-size: 120%"
+      >
+        Em 2023, o Brasil bateu o record de casos de estupro
+      </h3>
+    </v-card>
+    <v-card
+      color="#0E5BA7"
+      rounded="lg"
+      height="28vh"
+      style="text-align: center; align-content: center; "
+      width="70%"
+    >
+      <h3
+        style="background-color: #0E5BA7; color: #ffffff; font-size: 120%; padding: 5%"
+      >
+        De acordo com a Delegacia Especializada de Atendimento à Mulher (DEAM) de Contagem, nossa cidade ocupa o
+        segundo lugar em Minas Gerais no número de denúncias de violência contra mulheres e crianças. Apenas
+        Belo Horizonte, que tem uma população três vezes maior, apresenta um número mais alto de denúncias.
+      </h3>
+    </v-card>
+  </div>
+  <section
+    class="d-flex  justify-center align-center"
+  >
+    <img src="/src/assets/TiaKeylaAside.png"
+         alt="Imagem de Tia Keyla de braços cruzados"
+         style="
+          height: 100%;
+          width: 25%;
+        "
+    >
+    <img src="/src/assets/text.png"
+     alt="Texto: Com a ajuda de Deus e o seu apoio, trabalharei incansavelmente para diminuir tais incidências
+     em nossa Cidade, contribuindo efetivamente para a diminuição desta estatística no Brasil."
+    style="
+      height: 70%;
+      width: 50%;
+    "
+    >
+  </section>
+  <section
+    id="propostas"
+    style="
+      margin-top: 5%;
+    "
+  >
+    <h2 style="font-size: 250%; padding-left: 5%; padding-right: 5%; margin-bottom: 2%">Propostas</h2>
+    <div
+      style="
+        display: flex;
+        gap: 1%;
+        padding-left: 5%;
+        padding-right: 5%;
+      "
+      v-for="item in arrPropostas"
+      :key="item"
+    >
+      <v-icon icon="mdi-arrow-right-bold-circle" color="#f97a08" size="35"/>
+      <div
+        style="display: flex; flex-direction: column;"
+      >
+        <h4 style="font-weight: bold">{{ item.title}}</h4>
+        <p style="margin-bottom: 2%">{{ item.description }}</p>
+      </div>
+    </div>
+  </section>
+  <section style="margin-top: 2%;">
+    <h2 style="font-size: 300%; font-weight: bold; padding-left: 2%">Decreto Infância Protegida</h2>
+    <div class="d-flex" style="margin-top: 2%; margin-left: 5%">
+      <img src="/src/assets/TiaKeylaEJunio.png"
+           alt="Tia Keyla para vereadora (22.123) Junio Amaral para prefeito (22)"
+           style="height: 20%;
+                  width: 30%;
+           "
+      >
+      <div style="padding-left: 5%; padding-right: 5%; text-justify: inter-word">
+        <p>Em parceria e compromisso firmados com nosso <span class="span_strong">candidato a prefeitura de Contagem, Cabo Junio
+          Amaral,</span> traçamos um pacto a favor da infância contendo, não limitado, os seguintes tópicos:</p>
+        <ul
+          v-for="item in arrDecreto"
+          :key="item"
+        >
+          <li style="color: #0e5ba7">
+            {{ item.text }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <section class="d-flex justify-center flex-column align-center">
+    <h2 style="font-size: 280%; font-weight: bold; color: #f97a08; line-height: 1">Passeata Maio Laranja 2024</h2>
+    <h3 style="font-size: 170%; color: #f97a08">Conheça o Projeto #Protegidos - Clique aqui</h3>
+    <a href="https://www.youtube.com/watch?v=8EH2dck9Tdo" target="_blank">
+      <img src="/src/assets/video_protegidos.png"
+           style="width: 30vw; height: 40vh; margin-top: 2%;"
+      >
+    </a>
+    <p style="color: #f97a08; padding-top: 2%; padding-left: 10%; padding-right: 10%">
+      A  Passeata Maio Laranja, realizada em 11 de maio de 2024 na cidade de Contagem, foi um marco de solidariedade e
+      esperança na luta contra o abuso sexual infantil e de adolescentes. Centenas de pessoas se uniram, formando uma
+      corrente de coragem e determinação, ecoando vozes que há muito tempo precisam ser ouvidas. As ruas se encheram de
+      famílias, ativistas, profissionais de diversas áreas e cidadãos de coração aberto, todos caminhantes lado
+      a lado em prol de um futuro mais seguro para o nosso país.
+    </p>
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSf5uu3xm0aA90h_FuM_bQy7EXXXLlw8kUAmsUB8SxShmQ3iTg/viewform"
+       target="_blank"
+       style="text-decoration: none"
+    >
+      <v-card
+        color="#f97a08"
+        rounded="lg"
+        height="7vh"
+        style="text-align: center; align-content: center; margin-top: 20%"
+        width="120%"
+      >
+        <h3
+          style="background-color: #f97a08; font-weight: bold"
+        >
+          SEJA UM VOLUNTÁRIO
+        </h3>
+      </v-card>
+    </a>
+  </section>
+  <section>
+    <h2 style="font-size: 250%; margin-top: 5%" class="d-flex justify-center align-center">Depoimentos</h2>
+    <div class="d-flex justify-center align-center flex-column flex-sm-wrap" v-for="i in 4">
+      <img :src="`/src/assets/depoimento${i}.png`" style="width: 30%; margin-top: 5%;">
+    </div>
+  </section>
+  <section>
+    <h2 style="font-size: 250%; margin-top: 5%" class="d-flex justify-center align-center">Galeria</h2>
+    <h3>Memórias de momentos inesquecíveis, família e apoiadores da Tia Keyla.</h3>
+    <div class="d-flex justify-center align-center flex-column flex-sm-wrap" v-for="i in 12">
+      <img :src="`/src/assets/${i}..png`" style="width: 30%; margin-top: 5%;">
+    </div>
+  </section>
+  <footer class="d-flex align-center justify-space-between" style="padding: 0 5%; margin-top: 5%;" id="contatos">
+    <div style="width: 30%">
+      <h2 style="font-size: 250%; font-weight: bold">Entre em contato</h2>
+      <a href="https://chat.whatsapp.com/Id7JGzhEKnGA6pbpbSqmu3" style="text-decoration: none">
+        <section style="margin-top: 8%; display: flex; align-items: flex-start; justify-content: space-between">
+          <p style="font-weight: bold">GRUPO EXÉRCITO DA PREVENÇÃO</p>
+          <img src="/src/assets/whatsapp.png" alt="Icone do whatsapp">
+        </section>
+      </a>
+      <a href="https://www.instagram.com/tiakeyla" style="text-decoration: none">
+        <section style="margin-top: 5%; display: flex; align-items: flex-start; justify-content: space-between">
+          <div>
+            <p style="font-weight: bold">INSTAGRAM</p>
+            <p>@tiakeyla</p>
+          </div>
+          <img src="/src/assets/instagram.png" alt="Icone do instagram">
+        </section>
+      </a>
+      <a href="mailto:contato@tiakeylacontagem.com.br" style="text-decoration: none">
+        <section style="margin-top: 5%; display: flex; align-items: flex-start; justify-content: space-between">
+          <div>
+            <p style="font-weight: bold">E-MAIL</p>
+            <p>contato@tiakeylacontagem.com.br</p>
+          </div>
+          <img src="/src/assets/mail.png" alt="Icone de e-mail">
+        </section>
+      </a>
+      <section style="margin-top: 5%; display: flex; align-items: flex-start; justify-content: space-between">
+        <div>
+          <p>Propaganda Eleitoral</p>
+          <p>CNPJ 56.483.833/0001-05</p>
+          <p>Eleições 2024<br>Keyla Cristina Parreiras Pinto Aredes</p>
+        </div>
+        <div>
+          <img src="/src/assets/pl.png" alt="Logo do partido PL">
+        </div>
+      </section>
+    </div>
+    <div>
+      <img src="/src/assets/TiaKeylaFooter.png" alt="Imagem de Tia Keyla de braços cruzados" style="width: 100%; height: 80%">
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: "WebSite",
+  data() {
+    return {
+      arrPropostas: [
+        {
+          title: 'Efetivo enfrentamento a abusos e maus-tratos contra crianças e Adolescentes.',
+          description: `A proposta visa implementar uma abordagem robusta e sistemática para combater o abuso sexual
+          e os maus-tratos contra crianças e adolescentes. Isso inclui a criação de programas de conscientização e
+          prevenção nas escolas, capacitação contínua de profissionais de educação, saúde e segurança para
+          identificar e intervir precocemente em casos de abuso.`
+        },
+        {
+          title: 'Fortalecimento do conselho de direitos da criança e do adolescente.',
+          description: `fortalecimento das redes de proteção e a criação de canais de denúncias vulneráveis
+          e seguras, garantindo que as vítimas recebam o apoio necessário, como atendimento psicológico e jurídico,
+          e que os agressores sejam devidamente responsabilizados.
+          Também está incluída a proposta de políticas públicas para a reintegração e apoio às vítimas no seu
+          desenvolvimento pessoal e educacional`
+        },
+        {
+          title: 'Combater a disseminação de pornografia para crianças e adolescentes.',
+          description: `Estou preocupada com o impacto nocivo da pornografia na vida das nossas crianças e adolescentes.
+          Minha proposta é educar nossas escolas e comunidades sobre os perigos da exposição precoce a esses conteúdos,
+          e lutar por uma regulamentação rigorosa que proteja nossos jovens. Em parceria com as plataformas digitais,
+          quero assegurar que a infância permaneça protegida desse mal silencioso que ameaça o seu desenvolvimento
+          saudável.`
+        },
+        {
+          title: 'Buscar parcerias e recursos para a Educação Infantil.',
+          description: `A educação é o pilar para uma infância segura e promissora. Quero ir além das promessas e
+          buscar parcerias com o governo e a iniciativa privada para garantir que nossas creches e escolas sejam espaços
+          de excelência. Meu compromisso é assegurar profissionais capacitados, infraestrutura adequada e materiais
+          didáticos modernos, porque cada criança de Contagem merece o melhor início de vida, com uma educação que
+          a prepare para o futuro.`
+        },
+        {
+          title: 'Pela promoção de bons tratos na vida.',
+          description: `Minha missão é construir uma sociedade onde o respeito, o carinho e a empatia estejam no centro
+          da convivência familiar e comunitária. Quero trazer para Contagem programas que ensinem práticas de
+          bons-tratos, promovendo o bem-estar emocional e social das crianças e adolescentes. Com palestras,
+          workshops e campanhas, vamos juntos cultivar uma cultura de respeito e harmonia que começa nas famílias e se
+          espalha por toda a comunidade. Uma cidade onde cada criança é valorizada e protegida é uma cidade que avança.`
+        },
+        {
+          title: 'Por que votar em Tia Keyla?',
+          description: `Porque meu compromisso é com as crianças de Contagem. Minha experiência, dedicação e paixão pela
+           causa me motivam a lutar por uma cidade onde cada criança é protegida, valorizada e tem a oportunidade de
+            crescer em um ambiente saudável e seguro. Com seu voto, vamos garantir uma infância livre de abusos e
+            maus-tratos, e construir um futuro de dignidade e esperança para nossos jovens. Vote 22123, e faça parte
+            dessa transformação.`
+        },
+      ],
+
+      arrDecreto: [
+        {
+          text: `Escolas e serviços de saúde serão mais transparentes com as famílias sobre conteúdos a
+          ministrar a crianças e adolescentes`
+        },
+        {
+          text: `Servidores públicos municipais não poderão favorecer ou criticar religiões, enquanto estiverem
+          no exercício de suas funções.`
+        },
+        {
+          text: `As famílias serão respeitadas pelos servidores públicos em seus valores e crenças,
+          inclusive seus filhos menores.`
+        },
+        {
+          text: `Escolas poderão apresentar temas morais ou religiosos a alunos menores, desde que obtenham PRÉVIA
+          autorização das famílias dos alunos.`
+        },
+        {
+          text: `Proibido materiais didáticos com conteúdo violador de leis protetivas da infância.`
+        },
+      ]
+    }
+  }
+})
+</script>
+
+
+<style scoped>
+  * {
+    margin: 0;
+    padding: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+    box-sizing: border-box;
+    color: #0e5ba7;
+  }
+  h1 {
+    font-size: 300%;
+  }
+  .navbar_btn {
+    font-size: 100%
+  }
+  .span_strong {
+    font-weight: bold;
+  }
+</style>
